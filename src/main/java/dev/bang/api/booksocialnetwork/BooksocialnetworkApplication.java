b4f6544c4355e39ead5b58de8f52cc545b56f9ed
bang-dev <ecosystemprogramminglanguage@gmail.com>
2024-04-26 16:25:22 +0700

    Basic

---
+++ src/main/java/dev/bang/api/booksocialnetwork/BooksocialnetworkApplication.java
@@ -0,0 +1,13 @@
+package dev.bang.api.booksocialnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksocialnetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksocialnetworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/bang/api/booksocialnetwork/BooksocialnetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.bang.api.booksocialnetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksocialnetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
